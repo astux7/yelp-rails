2083aedfb3d50ec6e6b9399e21671883c83a1fe0
astux7 <Asta.bevainyte@gmail.com>
2014-01-19 19:48:58 +0000

    test passsed

--- yelp/app/controllers/restaurants_controller.rb
+++ yelp/app/controllers/restaurants_controller.rb
@@ -1,5 +1,17 @@
 class RestaurantsController < ApplicationController
 	def new
-		
+
+    end
+
+    def create  
+    	#raise params[:restaurant].inspect
+        Restaurant.create params[:restaurant].permit(:name, :description)
+    	redirect_to '/restaurants'
+
+    end
+
+    def index
+    	@restaurants = Restaurant.all
     end
 end
+
--- yelp/spec/features/create_restaurants_feature_spec.rb
+++ yelp/spec/features/create_restaurants_feature_spec.rb
@@ -6,7 +6,7 @@ describe 'creating restaurants' do
 			visit '/restaurants/new'
 			fill_in 'Name', with: 'McDonalds'
 			fill_in 'Description', with: "I'm lovin' it"
-			click_button 'Submit'
+			click_button 'Create Restaurant'
 
 			expect(current_path).to eq '/restaurants'
 			expect(page).to have_content 'McDonalds'
