3ce36807d61442021d67a33f4818b2aa6070f816
astux7 <Asta.bevainyte@gmail.com>
2014-01-19 21:27:16 +0000

    added test of edit/update

--- yelp/app/controllers/restaurants_controller.rb
+++ yelp/app/controllers/restaurants_controller.rb
@@ -13,5 +13,15 @@ class RestaurantsController < ApplicationController
     def index
     	@restaurants = Restaurant.all
     end
+
+    def edit 
+    	@restaurant = Restaurant.find params[:id]
+    end
+
+    def update
+        @restaurant = Restaurant.find params[:id]
+        @restaurant.update params[:restaurant].permit(:name, :description)
+        redirect_to '/restaurants'
+    end
 end
 
