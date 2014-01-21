9a55caf0313834a54459db6159c7e98937dbf60a
astux7 <Asta.bevainyte@gmail.com>
2014-01-21 13:24:21 +0000

    fixed reviews for uniq reviews

--- yelp/app/models/review.rb
+++ yelp/app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ActiveRecord::Base
 	belongs_to :restaurant
 	belongs_to :user
 	validates :rating, inclusion: {in: :valid_ratings.to_proc, message: 'is not a valid rating'}
-	
+	#if review exist with id restaurant we cannot leave a review
+	validates :restaurant_id, uniqueness: {message: 'Review already been written'}
 	def valid_ratings
 	    if restaurant.promoted?
 	      return 1..5
